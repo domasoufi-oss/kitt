e2681ea252aa874acb1dce22b25ccb9784894af8
RoundPlanet <roundplanet@example.com>
2026-01-13 21:04:15 +0100

    fixe minor issues

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,13 +1,11 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('TS is now working on Windows and Linux!');
-});
 
-// Use '0.0.0.0' to allow external traffic from your router
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Face is listening on all interfaces at port ${port}`);
+const PORT = 3000;
+const HOST = '0.0.0.0'; // THIS IS THE KEY
+
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
 });
